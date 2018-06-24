fd70b911a2e7459d470f1c4e228b3ff637e9934d
Matheus Gambarin <matheusgambarin@gmail.com>
2018-06-24 20:36:25 -0300

    init tcc python project

---
+++ teste.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def dobro(x):
+    return x*2
+
+    number = dobro(5)
+
+    print("dobro de number: ", number)
+    scouts_weights = np.array([-2.0, -5.0, 3.0, 7.0, -0.5])
